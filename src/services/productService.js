667055d39b5ad867d246542dd2a4482a1c29c3d0
Kmarcos <nivek012016@gmail.com>
2026-07-02 22:53:55 -0500

    Actualizar API de productos con URL de Render

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://aquariumstore-react-ap3-api.onrender.com/productos'
+const API_URL = 'https://aquariumstore-react-ap3.onrender.com/productos'
 
 export async function obtenerProductos() {
   try {
